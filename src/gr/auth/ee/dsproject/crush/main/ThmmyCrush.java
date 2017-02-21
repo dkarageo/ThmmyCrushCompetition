c31beeaa68f2c8c52b0995fd52ef95dc09f9aafe
dkgr_ser <dkgr_ser@outlook.com>
2017-02-21 22:46:13 +0200

    Added main method to source for building.

---
+++ src/gr/auth/ee/dsproject/crush/main/ThmmyCrush.java
@@ -0,0 +1,11 @@
+package gr.auth.ee.dsproject.crush.main;
+
+import gr.auth.ee.dsproject.crush.board.MainPlatform;
+
+public class ThmmyCrush {
+
+	public static void main(String[] args) {
+		MainPlatform.main(args);
+	}
+
+}
